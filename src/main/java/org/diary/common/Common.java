47eca7ba62d88751b3ee491887d2e1de0bf1b0c7
jeongseongsoo <ssjeong920801@gmail.com>
2020-04-23 15:20:14 +0900

    #

--- src/main/java/org/diary/common/Common.java
+++ src/main/java/org/diary/common/Common.java
@@ -3,6 +3,6 @@ package org.diary.common;
 public class Common {
 	
 	public static final int CNT = 8;
-	public static final String PATH = "/diary/";
+	public static final String PATH = "/home/ec2-user/app/git/diary/";
 	
 }
