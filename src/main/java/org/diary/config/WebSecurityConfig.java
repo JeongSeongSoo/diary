adabd21812bb0060966199f37bebeaaef771c699
jeongseongsoo <ssjeong920801@gmail.com>
2020-04-23 11:48:04 +0900

    #

--- src/main/java/org/diary/config/WebSecurityConfig.java
+++ src/main/java/org/diary/config/WebSecurityConfig.java
@@ -2,13 +2,11 @@ package org.diary.config;
 
 import org.diary.security.WebAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -17,11 +15,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	public WebAuthenticationProvider webAuthenticationProvider;
-	
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
 	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
